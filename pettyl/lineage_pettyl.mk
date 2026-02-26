b6537282f8011fa9290d89df47ff5b6d1cedc1bb
elmendezz <165435968+elmendezz@users.noreply.github.com>
2026-02-26 10:03:09 -0800

    Update lineage_pettyl.mk

--- pettyl/lineage_pettyl.mk
+++ pettyl/lineage_pettyl.mk
@@ -1,14 +1,7 @@
-# Version: 1
-# Changelog:
-# - Se movieron las definiciones de PRODUCT antes de las herencias comunes para asegurar consistencia.
-# - Se ajustó el orden de herencia de Lineage para evitar sobreescritura de variables de identidad.
-
-# Identidad del Producto (Definir antes de heredar de Lineage)
-PRODUCT_DEVICE := pettyl
-PRODUCT_NAME := lineage_pettyl
-PRODUCT_BRAND := motorola
-PRODUCT_MODEL := moto e5 play
-PRODUCT_MANUFACTURER := motorola
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
@@ -18,6 +11,12 @@ $(call inherit-product, device/motorola/pettyl/device.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_DEVICE := pettyl
+PRODUCT_NAME := lineage_pettyl
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := moto e5 play
+PRODUCT_MANUFACTURER := motorola
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
