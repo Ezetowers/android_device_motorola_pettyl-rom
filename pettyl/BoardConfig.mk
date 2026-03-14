ae9189aa551c7eaa87346fde1020accaf38c98f3
elmendezz <165435968+elmendezz@users.noreply.github.com>
2026-03-14 06:18:53 -0700

    update BoardConfig.mk

--- pettyl/BoardConfig.mk
+++ pettyl/BoardConfig.mk
@@ -14,7 +14,7 @@ TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := cortex-a53
 TARGET_CPU_VARIANT_RUNTIME := cortex-a53
-# TARGET_USES_64_BIT_BINDER := true
+TARGET_USES_64_BIT_BINDER := true
 
 # ==========================================================
 # Platform & Bootloader
