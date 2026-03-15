df8be26781bb003911b6ee7393a1a1f67ec8a7d4
elmendezz <angelgamerhaker@gmail.com>
2026-03-15 00:20:35 -0700

    pettyl: create /firmware directory to fix SELinux labeling error

--- pettyl/device.mk
+++ pettyl/device.mk
@@ -87,3 +87,7 @@ $(call inherit-product, vendor/motorola/pettyl/pettyl-vendor.mk)
 # =================================================================
 # =================================================================
 # =================================================================
+
+# Fix SELinux: Create /firmware mount point
+PRODUCT_COPY_FILES += \
+    device/motorola/pettyl/configs/.placeholder:system/firmware/.placeholder
